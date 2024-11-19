6777e7c3410b9cc8b7142825afad87c79a0a07e6
ayushadarshgit <143077526+ayushadarshgit@users.noreply.github.com>
2024-11-20 00:41:13 +0530

    Update axiosInstance.ts

--- client/src/api/axiosInstance.ts
+++ client/src/api/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:8000/api",
+    baseURL: "https://car-management-65lw.onrender.com/api",
     withCredentials: true
 })
 
